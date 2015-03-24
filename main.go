9780eb0912a7d09ad1880aefe96672ecc06a2ca8
inaimathi <inaimathi@self>
2015-03-23 23:21:24 -0400

    -Working on the wiki datatype thing

--- main.go
+++ main.go
@@ -8,17 +8,8 @@ import (
 func main() {
 	fmt.Println(os.Args) // command-line args
 	fmt.Println(os.Args[1:]) // as above, without program name
-	p1 := &Page{Path: "Welcome.md", Body: []byte("# This is a simple index page...")}
-	p1.save()
-	p2, _ := loadPage("Welcome.md")
-	fmt.Println(string(p2.render()))
-	file, _ := os.Open("Welcome.md")
-	fstat, _ := file.Stat()
-	fmt.Println(fstat.ModTime().String())
-	file.Close()
+	
+	// wik := &Wiki{"/home/inaimathi/tmp"}
+	// wik.edit("test.md", []byte("Testing testing, one two three"))
+	// wik.remove("test.md")
 }
-
-// func main() {
-// 	fmt.Println("Testing...")
-// 	fmt.Println("And, testing again and again")
-// }
--- wiki.go
+++ wiki.go
@@ -1,45 +1,72 @@
 package main
 
 import (
-	"os" 
-	"time"
+	"errors"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 	"io/ioutil"
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/russross/blackfriday"
 )
 
-type Page struct {
+type Wiki struct {
 	Path string
-	ModTime time.Time
-	Body []byte
 }
 
-func (p *Page) save() error {
-	return ioutil.WriteFile(p.Path, p.Body, 0600)
+func (w *Wiki) Raw(path string) ([]byte, error) {
+	return ioutil.ReadFile(path)
 }
 
-func (p *Page) render() []byte {
-	unsafe := blackfriday.MarkdownCommon(p.Body)
-	return bluemonday.UGCPolicy().SanitizeBytes(unsafe)
-}
-
-func (p *Page) check_file() {
-	file, _ := os.Open(p.Path)
-	defer file.Close()
-	fstat, _ := file.Stat()
-	if fstat.ModTime().After(p.ModTime) {
-		var dst []byte
-		file.Read(dst)
-		p.Body = dst
-		p.ModTime = fstat.ModTime()
-	}
-}
-
-func loadPage(title string) (*Page, error) {
-	body, err := ioutil.ReadFile(title)
+func (w *Wiki) Render(path string) ([]byte, error) {
+	body, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
 	}
-	
-	return &Page{Path: title, Body: body}, nil
+	unsafe := blackfriday.MarkdownCommon(body)
+	return bluemonday.UGCPolicy().SanitizeBytes(unsafe), nil
+}
+
+func (w *Wiki) Local(path string) (string, error) {
+	p := filepath.Clean(filepath.Join(w.Path, path))
+	if (strings.HasPrefix(p, w.Path)) {
+		return p, nil
+	}
+	return "", errors.New("path outside of repo")
+}
+
+func (w *Wiki) Create(path string) error {
+	p, err := w.Local(path)
+	if (err != nil) { return err }
+	return ioutil.WriteFile(p, []byte("# " + path), 0600)
+}
+
+func (w *Wiki) Edit(path string, contents []byte) error {
+	p, err := w.Local(path)
+	if (err != nil) { return err }
+	return ioutil.WriteFile(p, contents, 0600)
+}
+
+func (w *Wiki) Remove(path string) error {
+	p, err := w.Local(path)
+	if (err != nil) { return err }
+	os.Remove(p)
+	return nil
+}
+
+func (w *Wiki) ExecIn(command string, args ...string) error {
+	cmd := exec.Command(command, args...)
+	cmd.Dir = w.Path
+	return cmd.Run()
+}
+
+func (w *Wiki) Initialize() error {
+	return w.ExecIn("git", "init")
+}
+
+func (w *Wiki) Commit(path string, message string) error {
+	w.ExecIn("git", "add", "--all", path)
+	w.ExecIn("git", "commit", "-m", message)
+	return nil
 }
