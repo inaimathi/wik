aa04ed6e191c31c8287282de70fb662f5c7deb34
inaimathi <inaimathi@self>
2015-03-25 10:18:46 -0400

    -Now using html/templates

--- server.go
+++ server.go
@@ -2,62 +2,60 @@ package main
 
 import (
 	"net/http"
+	"html/template"
 )
 
 func WikiHandlers (wiki *Wiki) {
 	http.HandleFunc("/", ShowPage(wiki))
-	http.HandleFunc("/edit/", EditPage(wiki))
-	http.HandleFunc("/api/remove/", RemoveAPI(wiki))
-	http.HandleFunc("/api/edit/", EditAPI(wiki))
-	http.HandleFunc("/api/create/", CreateAPI(wiki))
+	http.HandleFunc("/edit/", ShowEdit(wiki))
+	http.HandleFunc("/api/remove/", RemovePage(wiki))
+	http.HandleFunc("/api/edit/", EditPage(wiki))
+	http.HandleFunc("/api/create/", CreatePage(wiki))
 }
 
 func ShowPage (wiki *Wiki) func (http.ResponseWriter, *http.Request) {
+	t, _ := template.ParseFiles("static/templates/show.html")
 	return func (w http.ResponseWriter, r *http.Request) {
 		body, err := wiki.Render(r.URL.Path)
-		if err == nil { WritePage(w, body) }
+		if err == nil { t.Execute(w, template.HTML(body)) }
 	}
 }
 
-func EditPage (wiki *Wiki) func (http.ResponseWriter, *http.Request) {
+func ShowEdit (wiki *Wiki) func (http.ResponseWriter, *http.Request) {
+	t, _ := template.ParseFiles("static/templates/edit.html")
 	return func (w http.ResponseWriter, r *http.Request) {
 		body, err := wiki.Raw(r.URL.Path[len("/edit/"):])
-		if err == nil { WritePage(w, body) }
+		if err == nil { t.Execute(w, template.HTML(body)) }
 	}
 }
 
-func RemoveAPI (wiki *Wiki) func (http.ResponseWriter, *http.Request) {
+func RemovePage (wiki *Wiki) func (http.ResponseWriter, *http.Request) {
 	return func (w http.ResponseWriter, r *http.Request) {
 		err := wiki.Remove(r.URL.Path[len("/api/remove/"):])
 		if err == nil {
-			http.Redirect(w, r, "/", 303)
+			http.Redirect(w, r, "/", http.StatusFound)
 		}
 	}
 }
 
-func CreateAPI (wiki *Wiki) func (http.ResponseWriter, *http.Request) {
+func CreatePage (wiki *Wiki) func (http.ResponseWriter, *http.Request) {
 	return func (w http.ResponseWriter, r *http.Request) {
 		path := r.URL.Path[len("/api/create/"):]
 		err := wiki.Create(path)
 		if err == nil {
-			http.Redirect(w, r, "/" + path, 303)
+			http.Redirect(w, r, "/" + path, http.StatusFound)
 		}
 	}
 }
 
-func EditAPI (wiki *Wiki) func (http.ResponseWriter, *http.Request) {
+func EditPage (wiki *Wiki) func (http.ResponseWriter, *http.Request) {
 	return func (w http.ResponseWriter, r *http.Request) {
 		path := r.URL.Path[len("/api/edit/"):]
 		r.ParseForm()
 		body := r.Form.Get("new_contents")
 		err := wiki.Edit(path, []byte(body))
 		if err == nil {
-			http.Redirect(w, r, "/" + path, 303)
+			http.Redirect(w, r, "/" + path, http.StatusFound)
 		}
 	}
-}
-
-func WritePage(w http.ResponseWriter, cont []byte) {
-	w.Header().Set("Content-Type", "text/html")
-	w.Write(PageWrapper(cont))
 }
