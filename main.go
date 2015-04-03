b104b83f0adb3f46ba6b8e204e9aa636909ab4b5
inaimathi <inaimathi@self>
2015-04-03 19:50:44 -0400

    -Added command-line parsing via flag module

--- main.go
+++ main.go
@@ -1,22 +1,23 @@
 package main
 
 import (
-	"os"
 	"fmt"
+	"flag"
 	"strings"
 	"net/http"
 )
 
 func main() {
-	wik := &Wiki{strings.TrimRight(os.Args[1], "/")}
+	var port = flag.Int("port", 8080, "Specify the TCP port this server should listen on. Defaults to 8080.")
+	flag.Parse()
+	wik := &Wiki{strings.TrimRight(flag.Arg(0), "/")}
 	WikiHandlers(wik)
 
-	fmt.Print("Serving ")
-	fmt.Println(os.Args[1])
-	fmt.Println("Listening on local port 8080...")	
+	fmt.Printf("Serving %s\n", flag.Arg(0))
+	fmt.Printf("Listening on local port %d...\n", *port)
 	
 	static := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", static))
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
 }
