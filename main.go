1feea52927036da2713b7697ca48f4eda6b5a306
inaimathi <inaimathi@self>
2015-04-03 18:13:52 -0400

    -main.go now uses the default http package static file serving facilities rather than shitty, hand-rolled ones

--- main.go
+++ main.go
@@ -4,14 +4,19 @@ import (
 	"os"
 	"fmt"
 	"net/http"
+	"io/ioutil"
 )
 
 func main() {
 	fmt.Println(os.Args) // command-line args
 	fmt.Println(os.Args[1:]) // as above, without program name
-	
+
 	wik := &Wiki{"/home/inaimathi/tmp"}
 
 	WikiHandlers(wik)
+	
+	static := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", static))
+
 	http.ListenAndServe(":8080", nil)
 }
